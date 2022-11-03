f98f4ad470fd33f5bb17b43ea24d81433f27c93f
cs2812 <cs786786786@gmail.com>
2022-11-03 21:00:10 +0530

    add todo

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express=require("express")
 const mongooes =require("mongoose")
 // const todoRoutes = require("./routes/todoRoutes")
 
-// const userRoute = require("./routes/userRoutes")
+const userRoute = require("./routes/userRoutes")
 const app = express()
 const port=3000
 
@@ -10,7 +10,7 @@ const port=3000
 app.use(express.urlencoded({extended:true}));
 app.use(express.json())
 
-// app.use("/user",userRoute)
+app.use("/user",userRoute)
 // app.use("/todo",todoRoutes)
 
 app.get("/",(req,res)=>{
