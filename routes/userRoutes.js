3bad9afe9364fcb98ce8e484eeb2e0bb20118398
cs2812 <cs786786786@gmail.com>
2022-11-03 20:38:42 +0530

    add todo route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -37,7 +37,7 @@ userRoute.post("/login",async(req,res)=>{
         { id:find._id, name:find.username, email:find.email },//you can give whatever you want to give or keep it empty
         "SECRET1234", // You can write whatever you want.
         { 
-            expiresIn:"1days" // token will be expire after 7days 
+            expiresIn:"1days" // token will be expire after 1days 
         })
 
         return res.send({ message:"Logged in successfull", token })
